7a5459e8a6089371ba8af6c9f7d8eb763afde405
chinmayee-tops <151346608+chinmayee-tops@users.noreply.github.com>
2024-02-21 10:31:02 +0530

    Add files via upload

---
+++ 3.53_Random_From_Range.py
@@ -0,0 +1,14 @@
+"""
+How can you pick a random item from a range?
+"""
+
+import random
+print('Random Numbers from 1 to 50 are:')
+
+
+x=random.randrange(1,50)
+print(x)
+
+
+y=random.randrange(1,50,2)
+print(y)
---
+++ 3.54_Random_Number.py
@@ -0,0 +1,7 @@
+"""
+How can you get a random number in python?
+"""
+
+import random
+
+print(random.randint(0,9))
